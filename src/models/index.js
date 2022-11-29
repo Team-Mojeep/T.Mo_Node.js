f5c59a347821dc8065d2bb3161e42a1ffdf6d8b2
soyeonkim0227 <soyeonkim0227@naver.com>
2022-11-29 17:13:33 +0900

    🗃 :: 테이블 관계성 설정

--- src/models/index.js
+++ src/models/index.js
@@ -7,8 +7,18 @@ const sequelize = new Sequelize({ ...config, sync: false });
 
 db.User = require("./user")(sequelize, Sequelize);
 db.Review = require("./review")(sequelize, Sequelize);
+db.Recruitment = require("./recruitment")(sequelize, Sequelize);
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
+
+db.User.hasMany(db.Review, { foreignKey: "user_id", targetKey: "id" });
+db.Review.belongsTo(db.User, { foreignKey: "user_id" });
+
+db.User.hasMany(db.Recruitment, { foreignKey: "user_id", targetKey: "id" });
+db.Recruitment.belongsTo(db.User, { foreignKey: "user_id" });
+
+db.Recruitment.hasMany(db.Review, { foreignKey: "recruitment_id", targetKey: "id" });
+db.Review.belongsTo(db.Recruitment, { foreignKey: "recruitment_id" });
 
 module.exports = db;
